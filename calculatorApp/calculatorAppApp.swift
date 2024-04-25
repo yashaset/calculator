38bfac13c1ee281d62cd3f1977e2621e5c9edc42
Yash <yash@Yashs-MacBook-Pro.local>
2024-04-25 19:39:16 +0530

    feat: initial setup

---
+++ calculatorApp/calculatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorAppApp.swift
+//  calculatorApp
+//
+//  Created by Yash on 25/04/24.
+//
+
+import SwiftUI
+
+@main
+struct calculatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
